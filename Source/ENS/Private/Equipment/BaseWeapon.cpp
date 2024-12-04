a92eff1c5e1af7ba832d3ac8436578b4eee8e986
GRANGER Kévin <kevin.granger@ynov.com>
2024-12-04 11:33:11 +0100

    temp

---
+++ Source/ENS/Private/Equipment/BaseWeapon.cpp
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2024-2025, Equipment'N Slash contributors. All rights reserved.
+#include "Equipment/BaseWeapon.h"
+
+// Sets default values
+ABaseWeapon::ABaseWeapon()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ABaseWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABaseWeapon::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/ENS/Public/Equipment/BaseWeapon.h
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2024-2025, Equipment'N Slash contributors. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "BaseWeapon.generated.h"
+
+UCLASS()
+class ENS_API ABaseWeapon : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ABaseWeapon();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
