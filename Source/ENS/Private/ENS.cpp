9c0f787d79f91f6ce3dfbb8bc61ca831da07841d
Fabian INGREMEAU <60473022+Tractorou24@users.noreply.github.com>
2025-01-30 19:37:35 +0100

    damage-during-attack (#21)

--- Source/ENS/Private/ENS.cpp
+++ Source/ENS/Private/ENS.cpp
@@ -1,6 +1,24 @@
 // Copyright (c) 2024-2025, Equipment'N Slash contributors. All rights reserved.
 
 #include "ENS.h"
+#include "Utils/DebugMessageLogRedirector.h"
+
 #include "Modules/ModuleManager.h"
+#include "Utils/DebugMessageLogRedirector.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, ENS, "ENS");
+
+struct FEnsModuleInitializer final
+{
+    inline static FDebugMessageLogRedirector OutputDevice;
+
+    FEnsModuleInitializer()
+    {
+        FOutputDeviceRedirector::Get()->AddOutputDevice(&OutputDevice);
+    }
+
+    ~FEnsModuleInitializer()
+    {
+        FOutputDeviceRedirector::Get()->RemoveOutputDevice(&OutputDevice);
+    }
+} GEnsModuleInitializer;
