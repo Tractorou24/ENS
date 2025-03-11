d7de6f2adb47a2df16ebf2f0dc0866c41ef388af
Fabian INGREMEAU <fabian.ingremeau@outlook.com>
2025-03-11 18:33:38 +0100

    Use new base class in inventory and weapon instead of parent UEnsGameplayAbilityBase

--- Source/ENS/Public/Equipment/BaseWeapon.h
+++ Source/ENS/Public/Equipment/BaseWeapon.h
@@ -43,10 +43,14 @@ public:
     float BaseAttackDestroyTime = 2.f;
 #pragma endregion
 
-#pragma region Ability
-    /// \brief The weapon's main special ability.
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Abilities");
-    TSubclassOf<UEnsGameplayAbilityBase> MainAbility;
+#pragma region Skills
+    /// \brief The weapon current level
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Skills", meta = (ClampMin = "1", ClampMax = "3"))
+    uint8 Level = 1;
+
+    /// \brief The weapon special ability.
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Skills");
+    TSubclassOf<class UEnsSkillBase> MainSkill;
 #pragma endregion
 
     /// \brief Component representing the weapon model.
