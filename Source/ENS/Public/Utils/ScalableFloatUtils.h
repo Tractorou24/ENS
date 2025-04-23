700bdf21aaf95e880e58b4a2add4149d817f586a
Dorian Fonseca <71253089+Dorian2002@users.noreply.github.com>
2025-04-23 11:09:08 +0200

    Add helpers to get the scaled value of a scalable float in BP

---
+++ Source/ENS/Public/Utils/ScalableFloatUtils.h
@@ -0,0 +1,28 @@
+// Copyright (c) 2024-2025, BloodTear contributors. All rights reserved.
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+
+#include "ScalableFloatUtils.generated.h"
+
+struct FScalableFloat;
+
+/**
+ * \brief Helper functions to export C++ only members of \ref FScalableFloat in blueprint.
+ */
+UCLASS()
+class ENS_API UScalableFloatUtils : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+public:
+    /**
+     * \brief Gets the value of the scalable float \p Value using its curve at \p Level. 
+     * \param Value The scalable float to get the value from.
+     * \param Level The level to get the associated value.
+     * \return The value at \ref Level from \p Value.
+     */
+    UFUNCTION(BlueprintCallable, Category = "Utils")
+    static float GetValueAtLevel(const FScalableFloat& Value, float Level);
+};
